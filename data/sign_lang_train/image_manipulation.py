2a2864a21aacb0987530ee20231505097d5d1f43
Dominik Schweigl <domnik.schweigl@gmail.com>
2025-06-06 12:10:08 +0200

    feat: add svm and random forest

--- data/sign_lang_train/image_manipulation.py
+++ data/sign_lang_train/image_manipulation.py
@@ -6,10 +6,13 @@ from PIL import Image
 # CONFIGURATION
 image_folder = "images"           # Folder containing original images
 label_csv = "labels.csv"         # CSV file without header
-output_csv = "labels_all2.csv"
-# target_labels = ["1", "2", "3", "5", "7", "a", "e", "f", "h", "m", "n", "o", "q", "r", "t", "w", "x"]                # Label you want to filter
-target_labels = ["8", "b", "d", "i", "j", "k", "s", "v", "y"] 
-output_folder = "manipulated_images" # Where to save matching images
+output_csv = "labels_manipulated.csv"
+
+rotate_90_labels = ['1', '2', '3', '5', '7', '8', 'a', 'd', 'e', 'f', 'h', 'k', 'm', 'n', 'o', 'q', 'r', 't', 'w', 'x']
+rotate_270_labels = ['1', '2', '3', '5', '7', 'a', 'e', 'f', 'h', 'm', 'n', 'o', 'q', 'r', 't', 'w', 'x']
+mirror_horizontal_labels = ['1', '2', '3', '5', '7', '8', 'a', 'b', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y']
+
+output_folder = "images_manipulated" # Where to save matching images
 apply_transformation = True       # Set to False to just copy
 
 # Make sure output folder exists
@@ -24,40 +27,34 @@ augmented_df = labels_df.copy().iloc[0:0]
 # Loop through and process images with the target label
 for idx, row in labels_df.iterrows():
     label = row['label']
-    if str(label) in target_labels:
-        filename = str(row["imagepath"]).split(".")[0]
-        src_path = os.path.join(image_folder, filename + ".jpg")
+    filename = str(row["imagepath"]).split(".")[0]
+    src_path = os.path.join(image_folder, filename + ".jpg")
 
-        if os.path.exists(src_path):
-            img = Image.open(src_path)
+    if os.path.exists(src_path):
+        img = Image.open(src_path)
+        img.save(os.path.join(output_folder, filename + ".jpg"))
+        augmented_df.loc[len(augmented_df)] = [label, filename + ".jpg"]
 
-            if apply_transformation:
-                img2 = img.transpose(Image.FLIP_LEFT_RIGHT) 
-                dst_path = os.path.join(output_folder, filename + "_flipped_lr.jpg")
-                img2.save(dst_path)
-                augmented_df.loc[len(augmented_df)] = [label, filename + "_flipped_lr.jpg"]
-                
-                # img2 = img.transpose(Image.FLIP_TOP_BOTTOM) 
-                # dst_path = os.path.join(output_folder, filename + "_flipped_tb.jpg")
-                # img2.save(dst_path)
-                # augmented_df.loc[len(augmented_df)] = [label, filename + "_flipped_tb.jpg"]
-                
-                img2 = img.rotate(90)
-                dst_path = os.path.join(output_folder, filename + "_rotated_90.jpg")
-                img2.save(dst_path)
-                augmented_df.loc[len(augmented_df)] = [label, filename + "_rotated_90.jpg"]
-                
-                # img2 = img.rotate(180)
-                # dst_path = os.path.join(output_folder,  filename + "_rotated_180.jpg")
-                # img2.save(dst_path)
-                # augmented_df.loc[len(augmented_df)] = [label, filename + "_rotate_180.jpg"]
-                
-                # img2 = img.rotate(270)
-                # dst_path = os.path.join(output_folder, filename +"_rotated_270.jpg")
-                # img2.save(dst_path)
-                # augmented_df.loc[len(augmented_df)] = [label, filename + "_rotate_270.jpg"]
+        if label in mirror_horizontal_labels:
+            img2 = img.transpose(Image.FLIP_LEFT_RIGHT)  # type: ignore
+            dst_path = os.path.join(output_folder, filename + "_flipped_lr.jpg")
+            img2.save(dst_path)
+            augmented_df.loc[len(augmented_df)] = [label, filename + "_flipped_lr.jpg"]
 
-        else:
-            print(f"Image not found: {src_path}")
+        if label in rotate_90_labels:
+            img2 = img.rotate(90)
+            dst_path = os.path.join(output_folder, filename + "_rotated_90.jpg")
+            img2.save(dst_path)
+            augmented_df.loc[len(augmented_df)] = [label, filename + "_rotated_90.jpg"]
+        
+        if label in rotate_270_labels:
+            img2 = img.rotate(270)
+            dst_path = os.path.join(output_folder, filename +"_rotated_270.jpg")
+            img2.save(dst_path)
+            augmented_df.loc[len(augmented_df)] = [label, filename + "_rotated_270.jpg"]
+
+    else:
+        print(f"Image not found: {src_path}")
 
 augmented_df.to_csv(output_csv, header=False, index=False)
+augmented_df.to_csv(os.path.join(output_folder, output_csv), header=False, index=False)
